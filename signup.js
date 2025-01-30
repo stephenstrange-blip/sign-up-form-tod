34c182ed6a2140301e1551598ff134ad49d4b663
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-01-30 19:29:43 +0800

    Chore: Add comments

--- signup.js
+++ signup.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form");
 const password = document.getElementById("password");
-const cfm_password = document.getElementById("cfm-password");
+const cfmPassword = document.getElementById("cfm-password");
 const cfmPasswordError = document.querySelector(".error.cfm");
 
 const phoneFields = document.querySelectorAll("div > input[type=tel]");
@@ -8,35 +8,44 @@ const phoneRegex = new RegExp("/[0-9]{4}-[0-9]{3}-[0-9]{4}/");
 const phoneError = document.querySelector(".error.phone");
 const phoneNum = [];
 
-
+// Password Mismatch handling
 form.addEventListener("input", (event) => {
     if (password.validity.valueMissing) {
         cfmPasswordError.textContent = "Invalid Password Input"
     }
-    else if (password.value != cfm_password.value) {
+    else if (password.value != cfmPassword.value) {
         cfmPasswordError.textContent = "Password does not match!"
-        cfm_password.style['boxShadow'] = "rgba(255, 115, 0, 0.8) 0 0 .4rem"
-        cfm_password.style['borderColor'] = "rgba(255, 115, 0, 0.8)"
+
+        // Setting each styling attribute individually (Method #1)
+        cfmPassword.style['boxShadow'] = "rgba(255, 115, 0, 0.8) 0 0 .4rem"
+        cfmPassword.style['borderColor'] = "rgba(255, 115, 0, 0.8)"
     }
     else {
         cfmPasswordError.textContent = "Passwords Match"
-        Object.assign(cfm_password.style, {
+
+        // Setting each styling attribute at once (Method #2)
+        Object.assign(cfmPassword.style, {
             borderColor: "rgba(0, 182, 0, 0.8)",
             boxShadow: "rgba(0, 182, 0, .8) 0 0 .4rem"
         })
     }
 })
 
+// Phone Number handling on submit when all phone fields combined
 form.addEventListener("submit", (event) => {
+    //  field inputs are initially separate when collected from the DOM
     let phoneNum = combinePhoneFields();
-    console.log(phoneNum);
-    console.log(phoneRegex.test(phoneNum));
+
+    //  TODO - Fix the regex matching function
+    //  It is finding a match (returning True) as early as 
+    //  the input is XXXX-.
     if (!phoneRegex.test(phoneNum)) {
         phoneError.textContent = "Invalid Pattern";
         event.preventDefault();
     }
 })
 
+// Phone Number handling on input for each separate field
 phoneFields.forEach((field) => {
     field.addEventListener("input", (event) => {
         if (field.validity.valueMissing) {
@@ -55,14 +64,15 @@ function combinePhoneFields() {
     phoneFields.forEach((field) => {
         phoneNum.push(field.value);
     })
+    //Pattern is XXXX-XXX-XXX
     let phone = phoneNum.join("-");
-    console.log(phone);
     return phone
 }
 
+// Text cursor "tabs" automatically to next 
+// phone field if current is filled to max
 const autoTab = (current, length, next) => {
     const currentInput = document.querySelector('.' + current)
-    // console.log(currentInput.value.length, current)
     if (currentInput.value.length === length) {
         document.querySelector("." + next).focus();
     }
